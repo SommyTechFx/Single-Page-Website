d4b26039b9f06c2bb7415dc4489a8bf850196c87
Aningwu chisom <152747011+SommyTechFx@users.noreply.github.com>
2024-07-01 12:51:15 +0100

    final correction

--- HNGTask2.js
+++ HNGTask2.js
@@ -1,16 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const timeElement = document.getElementById("time");
-  const dayElement = document.getElementById("day");
+  const time = document.getElementById("time");
+  const day = document.getElementById("day");
 
-  function updateTimeAndDay() {
-    const now = new Date();
-    const utcTime = now.toISOString().split("T")[1].split(".")[0];
-    const dayOfWeek = now.toLocaleDateString("en-US", { weekday: "short" });
+  function updateDayandTime() {
+    const nowDay = new Date();
+    const utcTime = nowDay.toISOString().split("T")[1].split(".")[0];
+    const dayOfWeek = nowDay.toLocaleDateString("en-US", { weekday: "short" });
 
-    timeElement.textContent = utcTime;
-    dayElement.textContent = dayOfWeek;
+    time.textContent = utcTime;
+    day.textContent = dayOfWeek;
   }
 
-  updateTimeAndDay();
-  setInterval(updateTimeAndDay, 1000);
+  updateDayandTime();
+  setInterval(updateDayandTime);
 });
